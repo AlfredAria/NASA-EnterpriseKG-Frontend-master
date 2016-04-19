8f385467d8b4433bd50b0c7edd47eeb8494c4833
AlfredAria <alfredaria2013@gmail.com>
2016-04-19 15:04:23 +0800

    forum testpage

--- app/controllers/ForumController.java
+++ app/controllers/ForumController.java
@@ -1,16 +1,15 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import models.PostTitle;
 import play.mvc.Controller;
 import play.mvc.Result;
-import utils.Constants;
-import utils.RESTfulCalls;
+import views.html.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import views.html.forumEntrance;
+import com.fasterxml.jackson.databind.JsonNode;
 
 /**
  * A forum micro service?
@@ -18,38 +17,81 @@ import views.html.forumEntrance;
  *
  */
 public class ForumController extends Controller {
+	public static List<PostTitle> fake = new ArrayList<PostTitle> (
+			Arrays.asList(
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle(),
+					new PostTitle()
+			));
 
 	public static Result getPosts () {
-        JsonNode node = RESTfulCalls.getAPI(Constants.URL_HOST
-                + Constants.CMU_BACKEND_PORT
-                + "/forum/getPost");
-        List<PostTitle> titles = new ArrayList<>();
-        if (node.isArray()) {
-            for (final JsonNode objNode : node) {
-//                objNode.findPath("ratings");
-                titles.add(new PostTitle(0, 0,
-                        objNode.findPath("postTitle").asText(),
-                        "Post"
-                ));
-            }
-        }
-
-		return ok(forumEntrance.render(getPagesHelper(20), titles));
-
+		return ok(forumEntrance.render(getPagesHelper(), getPostTitlesHelper(1)));
 	}
-	
+
 	public static Result getPostsAtPage (Integer page) {
-        return ok("0");
-//		return ok(forumEntrance.render(getPagesHelper(), getPostTitlesHelper(page)));
+		if (page < 1 || page > getTotalPagesHelper())
+			page = 1;
+		return ok(forumEntrance.render(getPagesHelper(), getPostTitlesHelper(page)));
 	}
 
-	public static List<Integer> getPagesHelper(int numPosts) {
-		int count = numPosts % 10;
-		List<Integer> numbers = new ArrayList<> (); 
+	public static int getTotalPagesHelper() {
+		return fake.size() % 10;
+	}
+
+	public static List<Integer> getPagesHelper() {
+		int count = getTotalPagesHelper();
+		List<Integer> numbers = new ArrayList<> ();
 		for (int i = 1; i <= count; i ++ ) {
 			numbers.add(i);
 		}
 		return numbers;
 	}
 
+	public static List<PostTitle> getPostTitlesHelper (int page) {
+		int start = (page - 1) * 10;
+		int end = start + 10;
+		return fake.subList(start, Math.min(end, fake.size()));
+	}
 }
